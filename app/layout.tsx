0e7a327e7b82a335bfb29a2e536ea21db15a9628
Ruben Martinez <rubeniv2002@gmail.com>
2026-03-12 14:55:51 -0500

    Greatly enlarge favicon artwork

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ export const metadata: Metadata = {
   description: "Authority. Systems. Control.",
   icons: {
     icon: [
-      { url: "/favicon.png?v=19", type: "image/png", sizes: "512x512" },
-      { url: "/favicon.ico?v=19", rel: "shortcut icon" },
+      { url: "/favicon.png?v=20", type: "image/png", sizes: "512x512" },
+      { url: "/favicon.ico?v=20", rel: "shortcut icon" },
     ],
-    apple: [{ url: "/favicon.png?v=19", sizes: "180x180", type: "image/png" }],
-    shortcut: ["/favicon.ico?v=19"],
+    apple: [{ url: "/favicon.png?v=20", sizes: "180x180", type: "image/png" }],
+    shortcut: ["/favicon.ico?v=20"],
   },
 };
 
