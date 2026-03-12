c05ba4f956c527952ae0f7eb35d90996b963c46d
Ruben Martinez <rubeniv2002@gmail.com>
2026-03-12 14:45:39 -0500

    Move favicon down more

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ export const metadata: Metadata = {
   description: "Authority. Systems. Control.",
   icons: {
     icon: [
-      { url: "/favicon.png?v=9", type: "image/png", sizes: "512x512" },
-      { url: "/favicon.ico?v=9", rel: "shortcut icon" },
+      { url: "/favicon.png?v=10", type: "image/png", sizes: "512x512" },
+      { url: "/favicon.ico?v=10", rel: "shortcut icon" },
     ],
-    apple: [{ url: "/favicon.png?v=9", sizes: "180x180", type: "image/png" }],
-    shortcut: ["/favicon.ico?v=9"],
+    apple: [{ url: "/favicon.png?v=10", sizes: "180x180", type: "image/png" }],
+    shortcut: ["/favicon.ico?v=10"],
   },
 };
 
