a66baaf81c5870e909657549df76c923e922fcfd
Ruben Martinez <rubeniv2002@gmail.com>
2026-03-12 14:44:16 -0500

    Adjust favicon vertical alignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ export const metadata: Metadata = {
   description: "Authority. Systems. Control.",
   icons: {
     icon: [
-      { url: "/favicon.png?v=7", type: "image/png", sizes: "512x512" },
-      { url: "/favicon.ico?v=7", rel: "shortcut icon" },
+      { url: "/favicon.png?v=8", type: "image/png", sizes: "512x512" },
+      { url: "/favicon.ico?v=8", rel: "shortcut icon" },
     ],
-    apple: [{ url: "/favicon.png?v=7", sizes: "180x180", type: "image/png" }],
-    shortcut: ["/favicon.ico?v=7"],
+    apple: [{ url: "/favicon.png?v=8", sizes: "180x180", type: "image/png" }],
+    shortcut: ["/favicon.ico?v=8"],
   },
 };
 
